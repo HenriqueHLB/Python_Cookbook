f17fa49fe0f49e35eb1ea0c4d139c95aa283d5ca
HenriqueHLB <henriquelborba@gmail.com>
2019-12-19 18:25:31 -0300

    Ver a skin de um usuário do Minecraft

---
+++ skin_minecraft.py
@@ -0,0 +1,9 @@
+nick = input('Nick: ')
+
+print('\n')
+print(f'Cabeça 2D: https://mc-heads.net/avatar/{nick}')
+print(f'Cabeça 3D: https://mc-heads.net/head/{nick}')
+print(f'Corpo 2D: https://mc-heads.net/player/{nick}')
+print(f'Corpo 3D: https://mc-heads.net/body/{nick}')
+print(f'Capa: https://mc-heads.net/cape/{nick}')
+print(f'Skin: https://mc-heads.net/skin/{nick}')
