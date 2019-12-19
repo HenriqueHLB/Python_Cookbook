da95642e12da54b120b6c214452f8f2eacd3fc39
Henrique Liedtke Borba <45269139+HenriqueHLB@users.noreply.github.com>
2019-12-19 18:10:59 -0300

    deletar declarar_codificacao.py

--- declarar_codificacao.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-
-
-'''
-O comentário especial na linha 1 declara a codificação. Ela deve ser uma das codecs válidas com suporte do Python.
-OBS.: Por padrão, arquivos fonte de Python são tratados com codificação UTF-8.
-'''
