c06d16a9fe70a23ca39f73a798c3512f5ff86b6a
HenriqueHLB <henriquelborba@gmail.com>
2019-12-19 18:18:56 -0300

    Utilização dos operadores do Python

---
+++ operadores.py
@@ -0,0 +1,12 @@
+x = 13
+y = 4
+
+print(f'X = {x}\nY = {y}\n')
+
+print('x + y =', x+y)
+print('x - y =', x-y)
+print('x * y =', x*y)
+print('x / y =', x/y)
+print('x // y =', x//y)
+print('x % y =', x%y)
+print('x ** y =', x**y)
